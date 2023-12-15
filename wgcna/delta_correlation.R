13d38df0e87e1918bc400bd12da45f4b15b94a9a
himangi2003 <38924721+himangi2003@users.noreply.github.com>
2023-12-14 16:17:01 -0800

    Add files via upload

--- wgcna/delta_correlation.R
+++ wgcna/delta_correlation.R
@@ -52,17 +52,24 @@ treatment_group <- c("2 µg ID93 + 5 µg GLA-SE (3 Vaccine Injections)",
                      "2 µg ID93 + 5 µg GLA-SE (2 Vaccine Injections)")
 
 
-wgcna <- read.csv("new_wgcna_module.csv")
+wgcna <- read.csv("X:/fast/gilbert_p/fg_data/SCRI/TBVPX-203/RNA/21Nov2023/wgcna/aligned_wgcna_module.csv")
 
-deg_gene_list_1 <- na.omit(wgcna$turquoise)
-deg_gene_list_2 <- na.omit(wgcna$brown)
-deg_gene_list_3 <- na.omit(wgcna$yellow)
-deg_gene_list_4 <- na.omit(wgcna$green)
-deg_gene_list_5 <- na.omit(wgcna$blue)
-deg_gene_list_6 <- na.omit(wgcna$red)
-deg_gene_list_7 <- na.omit(wgcna$black)
-deg_gene_list_8 <- na.omit(wgcna$pink)
 
+deg_gene_list_1 <- wgcna$turquoise[!wgcna$turquoise  %in% "" ]
+deg_gene_list_2 <- wgcna$brown[!wgcna$brown  %in% "" ]
+deg_gene_list_3 <- wgcna$yellow[!wgcna$yellow  %in% "" ]
+deg_gene_list_4 <- wgcna$green[!wgcna$green  %in% ""]
+deg_gene_list_5 <- wgcna$blue[!wgcna$blue  %in% ""]
+deg_gene_list_6 <- wgcna$red[!wgcna$red %in% ""]
+deg_gene_list_7 <- wgcna$black[!wgcna$black %in% ""]
+deg_gene_list_8 <- wgcna$pink[!wgcna$pink %in% ""]
+
+
+
+module <- c("turquoise","brown","yellow","green","blue","red","black","pink")
+deg_modules <- list(deg_gene_list_1,deg_gene_list_2,deg_gene_list_3,
+                    deg_gene_list_4,deg_gene_list_5,deg_gene_list_6, 
+                    deg_gene_list_7,deg_gene_list_8)
 
 ################################################## 0 3 difference ##########################################################################################
 time_list <- "0"
@@ -89,6 +96,10 @@ colnames(dt_n) <- ptid_0_3_ni
 dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_0_3)))
 rownames(dt) <- my_list_genes
 colnames(dt) <- ptid_0_3
+
+
+delta_B_0_3 <- data.frame()
+delta_A_0_3 <- data.frame()
 for ( i in ptid_0_3)
 {
   sample_name_0 = meta_data %>%
@@ -101,13 +112,29 @@ for ( i in ptid_0_3)
     dplyr::filter(day %in% c("3")) %>%
     pull(samplename)
   
-  time_3_sample <- norm_counts_deg[,sample_name_3]
-  time_0_sample <- norm_counts_deg[,sample_name_0]
+  time_3_sample <- c(norm_counts_deg[,sample_name_3])
+  time_0_sample <- c(norm_counts_deg[,sample_name_0])
   dt[i]<-c(time_3_sample-time_0_sample)
-
+  dt_0_3<-cbind(dt,dt_n)
+  data_n <- list()
+  for (j in 1: length(deg_modules))
+  {
+    delta_B_0_3[j,i] <- c(mean(norm_counts[deg_modules[[j]],sample_name_3])-mean(norm_counts[deg_modules[[j]],sample_name_0]))
+    dt_0_3_mod <- dt[deg_modules[[j]],]
+    mod_0_3<-c(colMeans(dt_0_3_mod))
+    data_n[[j]] <-c(mod_0_3) 
+  }
+  delta_A_0_3 <- as.data.frame(do.call(rbind, data_n))
+  rownames(delta_B_0_3)<-module
+  rownames(delta_A_0_3)<-module
+  
 }
+  
 
-dt_0_3<-cbind(dt,dt_n)
+
+
+
+
 
 ################################################## 56 59 difference ##########################################################################################
 
@@ -133,198 +160,13 @@ colnames(dt_n) <- ptid_56_59_ni
 dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_59)))
 rownames(dt) <- my_list_genes
 colnames(dt) <- ptid_56_59
+
+delta_B_56_59 <- data.frame()
+delta_A_56_59 <- data.frame()
 for ( i in ptid_56_59)
 {
   sample_name_56 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
-    dplyr::filter(day %in% c("56")) %>%
-    pull(samplename)
-  
-  sample_name_59 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
-    dplyr::filter(day %in% c("59")) %>%
-    pull(samplename)
-  
-  time_59_sample <- norm_counts_deg[,sample_name_59]
-  time_56_sample <- norm_counts_deg[,sample_name_56]
-  dt[i]<-c(time_59_sample-time_56_sample)
-  
-}
-
-dt_56_59<-cbind(dt,dt_n)
-
-################################################## 56 59 difference ##########################################################################################
-
-ptid_56 = meta_data %>%
-  dplyr::filter(day %in% "56") %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-ptid_63 = meta_data %>%
-  dplyr::filter(day %in% "63") %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-ptid_56_63 <- ptid_63[(ptid_63 %in% ptid_56)]
-ptid_56_63_ni <- ptid_63[!(ptid_63 %in% ptid_56)]
-
-
-dt_n <-data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_63_ni)))
-rownames(dt_n) <- my_list_genes
-colnames(dt_n) <- ptid_56_63_ni
-dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_63)))
-rownames(dt) <- my_list_genes
-colnames(dt) <- ptid_56_63
-for ( i in ptid_56_63)
-{
-  sample_name_56 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
-    dplyr::filter(day %in% c("56")) %>%
-    pull(samplename)
-  
-  sample_name_63 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
-    dplyr::filter(day %in% c("63")) %>%
-    pull(samplename)
-  
-  time_63_sample <- norm_counts_deg[,sample_name_63]
-  time_56_sample <- norm_counts_deg[,sample_name_56]
-  dt[i]<-c(time_63_sample-time_56_sample)
-  
-}
-
-dt_56_63<-cbind(dt,dt_n)
-
-###############################################################################################################################################################################################################
-
-
-module <- c("turquoise","brown","yellow","green","blue","red","black","pink")
-deg_modules <- list(deg_gene_list_1,deg_gene_list_2,deg_gene_list_3,
-                    deg_gene_list_4,deg_gene_list_5,deg_gene_list_6, 
-                    deg_gene_list_7,deg_gene_list_8)
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_0_3_mod <- dt_0_3[gene,]
-  mod_0_3<-colMeans(dt_0_3_mod)
-  data_n[[i]] <-c(mod_0_3) 
-  
-}
-
-delta_A_0_3 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_0_3)<-module
-
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_56_59_mod <- dt_0_3[gene,]
-  mod_56_59<-colMeans(dt_56_59_mod)
-  data_n[[i]] <-c(mod_56_59) 
-  
-}
-
-delta_A_56_59 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_56_59)<-module
-
-
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_56_63_mod <- dt_0_3[gene,]
-  mod_56_63<-colMeans(dt_56_63_mod)
-  data_n[[i]] <-c(mod_56_63) 
-  
-}
-
-delta_A_56_63 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_56_63)<-module
-
-############################################################################################################################################################################################
-
-################################################## 0 3 difference ##########################################################################################
-time_list <- "0"
-ptid_0 = meta_data %>%
-  dplyr::filter(day %in% time_list) %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-time_list <- "3"
-ptid_3 = meta_data %>%
-  dplyr::filter(day %in% time_list) %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-ptid_0_3 <- as.character(ptid_0[(ptid_0 %in% ptid_3)])
-ptid_0_3_ni <- as.character(ptid_0[!(ptid_0 %in% ptid_3)])
-
-
-dt_n <-data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_0_3_ni)))
-rownames(dt_n) <- my_list_genes
-colnames(dt_n) <- ptid_0_3_ni
-dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_0_3)))
-rownames(dt) <- my_list_genes
-colnames(dt) <- ptid_0_3
-for ( i in ptid_0_3)
-{
-  sample_name_0 = meta_data %>%
     dplyr::filter(ptid == i) %>%
-    dplyr::filter(day %in% c("0")) %>%
-    pull(samplename)
-  
-  sample_name_3 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
-    dplyr::filter(day %in% c("3")) %>%
-    pull(samplename)
-  
-  time_3_sample <- norm_counts_deg[,sample_name_3]
-  time_0_sample <- norm_counts_deg[,sample_name_0]
-  dt[i]<-c(time_3_sample-time_0_sample)
-
-}
-
-dt_0_3<-cbind(dt,dt_n)
-
-################################################## 56 59 difference ##########################################################################################
-
-ptid_56 = meta_data %>%
-  dplyr::filter(day %in% "56") %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-ptid_59 = meta_data %>%
-  dplyr::filter(day %in% "59") %>%
-  dplyr::filter(Treatment_Group %in% treatment_group ) %>%
-  pull(ptid)
-
-
-ptid_56_59 <- ptid_59[(ptid_59 %in% ptid_56)]
-ptid_56_59_ni <- ptid_59[!(ptid_59 %in% ptid_56)]
-
-
-dt_n <-data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_59_ni)))
-rownames(dt_n) <- my_list_genes
-colnames(dt_n) <- ptid_56_59_ni
-dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_59)))
-rownames(dt) <- my_list_genes
-colnames(dt) <- ptid_56_59
-for ( i in ptid_56_59)
-{
-  sample_name_56 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
     dplyr::filter(day %in% c("56")) %>%
     pull(samplename)
   
@@ -333,13 +175,25 @@ for ( i in ptid_56_59)
     dplyr::filter(day %in% c("59")) %>%
     pull(samplename)
   
-  time_59_sample <- norm_counts_deg[,sample_name_59]
-  time_56_sample <- norm_counts_deg[,sample_name_56]
+  time_59_sample <- c(norm_counts_deg[,sample_name_59])
+  time_56_sample <- c(norm_counts_deg[,sample_name_56])
   dt[i]<-c(time_59_sample-time_56_sample)
+  dt_56_59<-cbind(dt,dt_n)
+  data_n <- list()
+  for (j in 1: length(deg_modules))
+  {
+    delta_B_56_59[j,i] <- c(mean(norm_counts[deg_modules[[j]],sample_name_59])-mean(norm_counts[deg_modules[[j]],sample_name_56]))
+    dt_56_59_mod <- dt[deg_modules[[j]],]
+    mod_56_59<-c(colMeans(dt_56_59_mod))
+    data_n[[j]] <-c(mod_56_59) 
+  }
+  delta_A_56_59 <- as.data.frame(do.call(rbind, data_n))
+  rownames(delta_B_56_59)<-module
+  rownames(delta_A_56_59)<-module
   
 }
 
-dt_56_59<-cbind(dt,dt_n)
+
 
 ################################################## 56 59 difference ##########################################################################################
 
@@ -365,10 +219,13 @@ colnames(dt_n) <- ptid_56_63_ni
 dt <- data.frame(matrix(nrow = length(my_list_genes), ncol = length(ptid_56_63)))
 rownames(dt) <- my_list_genes
 colnames(dt) <- ptid_56_63
+
+delta_B_56_63 <- data.frame()
+delta_A_56_63 <- data.frame()
 for ( i in ptid_56_63)
 {
   sample_name_56 = meta_data %>%
-    dplyr::filter(ptid ==  i) %>%
+    dplyr::filter(ptid == i) %>%
     dplyr::filter(day %in% c("56")) %>%
     pull(samplename)
   
@@ -377,63 +234,23 @@ for ( i in ptid_56_63)
     dplyr::filter(day %in% c("63")) %>%
     pull(samplename)
   
-  time_63_sample <- norm_counts_deg[,sample_name_63]
-  time_56_sample <- norm_counts_deg[,sample_name_56]
+  time_63_sample <- c(norm_counts_deg[,sample_name_63])
+  time_56_sample <- c(norm_counts_deg[,sample_name_56])
   dt[i]<-c(time_63_sample-time_56_sample)
+  dt_56_63<-cbind(dt,dt_n)
+  data_n <- list()
+  for (j in 1: length(deg_modules))
+  {
+    delta_B_56_63[j,i] <- c(mean(norm_counts[deg_modules[[j]],sample_name_63])-mean(norm_counts[deg_modules[[j]],sample_name_56]))
+    dt_56_63_mod <- dt[deg_modules[[j]],]
+    mod_56_63<-c(colMeans(dt_56_63_mod))
+    data_n[[j]] <-c(mod_56_63) 
+  }
+  delta_A_56_63 <- as.data.frame(do.call(rbind, data_n))
+  rownames(delta_B_56_63)<-module
+  rownames(delta_A_56_63)<-module
   
 }
-
-dt_56_63<-cbind(dt,dt_n)
 
 ###############################################################################################################################################################################################################
 
-
-module <- c("turquoise","brown","yellow","green","blue","red","black","pink")
-deg_modules <- list(deg_gene_list_1,deg_gene_list_2,deg_gene_list_3,
-                    deg_gene_list_4,deg_gene_list_5,deg_gene_list_6, 
-                    deg_gene_list_7,deg_gene_list_8)
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_0_3_mod <- dt_0_3[gene,]
-  mod_0_3<-colMeans(dt_0_3_mod)
-  data_n[[i]] <-c(mod_0_3) 
-  
-}
-
-delta_A_0_3 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_0_3)<-module
-
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_56_59_mod <- dt_0_3[gene,]
-  mod_56_59<-colMeans(dt_56_59_mod)
-  data_n[[i]] <-c(mod_56_59) 
-  
-}
-
-delta_A_56_59 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_56_59)<-module
-
-
-
-data_n <-list()
-for(i in 1:length(deg_modules))
-  
-{ 
-  gene = na.omit(na.omit(deg_modules[[i]]))
-  dt_56_63_mod <- dt_0_3[gene,]
-  mod_56_63<-colMeans(dt_56_63_mod)
-  data_n[[i]] <-c(mod_56_63) 
-  
-}
-
-delta_A_56_63 <- as.data.frame(do.call(rbind, data_n))
-rownames(delta_A_56_63)<-module
